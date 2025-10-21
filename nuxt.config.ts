51286ac026ae2cb12134f63646c547c5b8295d24
lukharsch <lukasharsch@web.de>
2025-10-21 12:15:22 +0200

    disable ssr for static site

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,13 +2,15 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/fonts'],
-  css: ['~/assets/css/main.css'],
+  modules: ["@nuxt/eslint", "@nuxt/fonts"],
+  css: ["~/assets/css/main.css"],
   vite: {
-    plugins: [
-      tailwindcss(),
-    ],
+    plugins: [tailwindcss()],
   },
-})
+  ssr: false,
+  nitro: {
+    preset: "static",
+  },
+});
